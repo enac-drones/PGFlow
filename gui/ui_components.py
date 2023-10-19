26d5a826210d5ddae30aef09835a39f2ea6091bc
Adrian del Ser <adrian.delser@gmail.com>
2023-10-19 13:58:17 +0200

    small mod

--- gui/ui_components.py
+++ gui/ui_components.py
@@ -13,17 +13,17 @@ class UIComponents(Observable):
         self.ax_build = self.fig.add_axes(
             [0.01, 0.01, 0.15, 0.05]
         )  # Position of 'Build Mode' button
-        # self.ax_drone = self.fig.add_axes(
-        #     [0.12, 0.01, 0.1, 0.05]
-        # )  # Position of 'Drone Mode' button
-        self.ax_reset = self.fig.add_axes([0.23, 0.01, 0.1, 0.05])  # Position of 'Reset' button
+        self.ax_run = self.fig.add_axes(
+            [0.17, 0.01, 0.1, 0.05]
+        )  # Position of 'Drone Mode' button
+        self.ax_reset = self.fig.add_axes([0.28, 0.01, 0.1, 0.05])  # Position of 'Reset' button
 
         self.btn_build = plt.Button(self.ax_build, "Switch Mode")
-        # self.btn_drone = plt.Button(self.ax_drone, "Drone Mode")
+        self.btn_run = plt.Button(self.ax_run, "Run")
         self.btn_reset = plt.Button(self.ax_reset, "Reset")
 
         self.btn_build.on_clicked(self.on_build_mode)
-        # self.btn_drone.on_clicked(self.on_drone_mode)
+        self.btn_run.on_clicked(self.on_run)
         self.btn_reset.on_clicked(self.on_reset)
 
     def on_build_mode(self, event):
@@ -33,4 +33,7 @@ class UIComponents(Observable):
 
     def on_reset(self, event):
         self.notify_observers("reset")
+
+    def on_run(self,event):
+        self.notify_observers("run")
 
